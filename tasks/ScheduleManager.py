7f8fd2930f215b9f1885a42b32fbfd7e56003a9e
jake-jlawson <jake.jlawson@gmail.com>
2024-10-10 13:05:03 -0500

    began implementing schedule management functionality

---
+++ tasks/ScheduleManager.py
@@ -0,0 +1,19 @@
+"""
+    SCHEDULE MANAGER MODULE
+"""
+# File:            || ScheduleManager.py ||
+# Description:     || Module for managing my calendar and tutoring schedule ||
+
+
+#CLASS: Calendar
+#Description: Class used to interface directly with google calendar
+class Calendar:
+    def __init__():
+        pass
+
+
+
+
+
+# Module Test Code
+print("Schedule Manager Test Running...")

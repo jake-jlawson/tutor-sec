5804188b1b73396cfd79d4f56871960713e66eb8
jake-jlawson <jake.jlawson@gmail.com>
2024-10-31 21:18:13 +0000

    began basic ui implementation using local flask server

--- ui/app.py
+++ ui/app.py
@@ -2,7 +2,7 @@
     UI ENTRY POINT
     Run this to start the UI
 """
-
+# IMPORTS
 from flask import Flask, render_template, request, jsonify
 import webbrowser, threading, os, subprocess, signal
 
@@ -11,14 +11,13 @@ APP = Flask(__name__)
 PORT = 5000
 
 
-# Define your main route
+# PRIMARY ROUTES
+# Main route
 @APP.route('/')
 def index():
     return render_template('index.html')
 
-
-
-# Route to shut down the server
+# Shutdown route
 @APP.route('/shutdown', methods=['POST'])
 def shutdown():
     print("Shutting down the server...")
@@ -26,16 +25,18 @@ def shutdown():
     return '', 204  # No content response
 
 
+
 # Open the UI in a new tab
 def open_ui():
     # register edge as the browser to use
     webbrowser.register('edge', None, webbrowser.BackgroundBrowser("C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe"))
-    
+
     # open the ui in a new tab
     webbrowser.get('edge').open_new_tab(f'http://localhost:{PORT}')
 
 
 
+# MAIN
 if __name__ == '__main__':
     threading.Timer(1, open_ui).start()
     APP.run(debug=False, port=PORT)
